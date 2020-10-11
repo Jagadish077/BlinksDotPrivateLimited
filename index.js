2db6e2a7355fdcf0848388de872cf6ee65d67dd6
Jagadish077 <jagadishjaga772@gmail.com>
2020-10-12 02:48:37 +0530

    initial-commit

--- index.js
+++ index.js
@@ -27,13 +27,13 @@ var https_options = {
     p7b: fs.readFileSync("./sll/www_blinksdot_com.p7b")
    };
 
-   const hostname = 'www.blinksdot.com';
-   app.use((req, res, next) => {
-       if(req.protocol === 'http'){
-           res.redirect(301, `https://${hostname}${req.headers.host}`);
-       }
-       next();
-   })
+//    const hostname = 'www.blinksdot.com';
+//    app.use((req, res, next) => {
+//        if(req.protocol === 'http'){
+//            res.redirect(301, `https://${hostname}${req.headers.host}`);
+//        }
+//        next();
+//    })
 
 app.use('/', covidData)
 
